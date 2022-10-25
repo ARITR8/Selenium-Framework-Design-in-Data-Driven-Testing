6107555948f59b86cac996e301981be832b2d808
Ari <74683543+gamrdrm@users.noreply.github.com>
2022-10-25 10:15:29 +0530

    Jenkins linked
    Jenkins scm configuration

--- Chapter10/Global_VARS.java
+++ Chapter10/Global_VARS.java
@@ -20,12 +20,12 @@ public class Global_VARS {
     // suite folder defaults
     public static String SUITE_NAME = null;
     public static final String TARGET_URL = "http://www.practiceselenium.com/";
-    public static String propFile = "src/main/java/com/framework/ux/utils/chapter10/selenium.properties";
+    public static String propFile = "/selenium-java/selenium.properties";
     public static final String SE_PROPS = new File(propFile).getAbsolutePath();
     public static final String TEST_OUTPUT_PATH = "test-output/";
     public static final String LOGFILE_PATH = TEST_OUTPUT_PATH + "Logs/";
     public static final String REPORT_PATH = TEST_OUTPUT_PATH + "Reports/";
-    public static final String REPORT_CONFIG_FILE = "src/main/java/com/framework/ux/utils/chapter10/extent-config.xml";
+    public static final String REPORT_CONFIG_FILE = "/selenium-java/extent-config.xml";
 
     // suite timeout defaults
     public static final int TIMEOUT_MINUTE = 60;
---
+++ Chapter10/zz/GoogleDynamicSearch.java
@@ -0,0 +1,26 @@
+
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+
+public class GoogleDynamicSearch {
+
+public static void main(String[] args) {
+	
+	//Set the google property
+	
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\Aritra\\Downloads\\testng-over-cucumber-parallelization-master\\testng-over-cucumber-parallelization-master\\chromedriver.exe");
+	
+	WebDriver driver = new ChromeDriver();
+	
+	driver.get("www.google.com");
+	
+	
+}	
+	
+	//set the chrome property
+	
+
+	
+
+}
